e72c85f519b707b346903b0697b8239c9ec55d97
Дмитрий Матвейчук <matvechuk74@gmail.com>
2022-10-23 03:13:47 +0300

    api_final

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -6,7 +6,11 @@ from .views import (
 from rest_framework_simplejwt.views import (
     TokenObtainPairView, TokenRefreshView, TokenVerifyView
 )
-
+jwt_patterns = [
+    path("verify/", TokenVerifyView.as_view(), name='token_verify'),
+    path("create/", TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path("refresh/", TokenRefreshView.as_view(), name='token_refresh'),
+]
 v1_router = DefaultRouter()
 v1_router.register(r"posts", PostViewSet)
 v1_router.register(r"groups", GroupViewSet)
@@ -19,10 +23,5 @@ v1_router.register(
 urlpatterns = [
     path("v1/", include(v1_router.urls)),
     path("v1/", include("djoser.urls")),
-    path(
-        'v1/jwt/create/', TokenObtainPairView.as_view(),
-        name='token_obtain_pair'
-    ),
-    path('v1/jwt/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('v1/jwt/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path("v1/jwt/", include(jwt_patterns)),
 ]
